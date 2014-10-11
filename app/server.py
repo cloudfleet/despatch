cba327e4eeb8dc0ca3657b4e1d0b8f35089a5823
DoubleMalt <christoph@web.crofting.com>
2014-10-11 18:16:12 +0200

    add more logging

--- app/server.py
+++ app/server.py
@@ -11,9 +11,12 @@ log.level = logging.DEBUG
 @stateless
 @nolocking
 def START(message, inbox=None, domain=None):
+
+    log.info("===============================")
+    log.info("received mail for %s@%s" % (inbox, domain))
     target_url = "http://" + domain + "/mailbox/raw/" + inbox  # FIXME change to https
     r = requests.post(target_url, headers={"Content-transfer-encoding": "binary"}, data=message.to_message().as_string())
-    log.info(r.text)
+    log.info("Server Response: %s" % r.text)
 
 
 
