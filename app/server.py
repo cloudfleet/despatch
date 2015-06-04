ae4c40349f9f94b1188144fb696a777672fb9086
Christoph Witzany <christoph@web.crofting.com>
2015-06-04 16:39:56 +0200

    send postmaster@<host> to cloudfleet to verify domain for StartSSL

--- app/server.py
+++ app/server.py
@@ -1,11 +1,21 @@
 import requests
 from salmon.routing import nolocking, route, stateless
+from salmon.mail include MailResponse
+from config import settings
 import json
 
 import logging
 
 log = logging.getLogger(__name__)
 log.level = logging.DEBUG
+
+@route("postmaster@(domain)", inbox=".+", domain=".+")
+@stateless
+def forward_postmaster(message, to=None, host=None):
+    logging.debug("MESSAGE to %s@%s forwarded to the relay host.", to, host)
+    settings.relay.deliver(MailResponse(To='admiralty@cloudfleet.io', From=message.From, Subject="[%s] %s" % (host, message.Subject), Body=message.body()))
+
+
 
 @route("(inbox)@(domain)", inbox=".+", domain=".+")
 @stateless
@@ -17,8 +27,3 @@ def START(message, inbox=None, domain=None):
     target_url = "http://blimp." + domain + "/mailbox/raw/" + inbox  # FIXME change to https
     r = requests.post(target_url, headers={"Content-transfer-encoding": "binary"}, data=message.to_message().as_string())
     log.info("Server Response: %s" % r.text)
-
-
-
-
-
--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,12 @@
-from salmon.server import SMTPReceiver
+from salmon.server import SMTPReceiver, Relay
+from os import environ
+
+RELAY_USERNAME=environ['MAILHOST_USERNAME']
+RELAY_PASSWORD=environ['MAILHOST_PASSWORD']
+RELAY_HOST=environ.get('MAILHOST_HOST', '127.0.0.1')
+RELAY_PORT=environ.get('MAILHOST_PORT', '25')
+RELAY_SSL=environ.get('MAILHOST_SSL', 'false')
 
 receiver = SMTPReceiver('0.0.0.0',25)
+relay = Relay(host=RELAY_HOST, port=int(RELAY_PORT), username=RELAY_USERNAME, password=RELAY_PASSWORD,
+                 ssl=RELAY_SSL.lower() == 'true', starttls=False, debug=0)
